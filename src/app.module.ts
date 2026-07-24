d4bd8e6b7fe679a05f4b1cb6882c40b667d73789
Omar Angarita <omar.angarita.j@gmail.com>
2026-07-23 21:35:52 -0500

    feat(app): registrar los módulos de users, grupos y unidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config';
 import { DatabaseModule } from './database/database.module';
+import { GruposModule } from './grupos/grupos.module';
 import { UnidadesModule } from './unidades/unidades.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, UnidadesModule],
+  imports: [
+    AppConfigModule,
+    DatabaseModule,
+    UsersModule,
+    GruposModule,
+    UnidadesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
